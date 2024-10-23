be07c4f94c700382eb4efedac8b5592e512858d3
Maye <maye.hunt@outlook.com>
2024-10-23 22:35:26 +0100

    added POST functionality for json body header

--- FizzBuzzApp.Api/FizzBuzzFunction.cs
+++ FizzBuzzApp.Api/FizzBuzzFunction.cs
@@ -1,7 +1,9 @@
 using FizzBuzzApp.Domain.Interfaces;
+using FizzBuzzApp.Domain.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Newtonsoft.Json;
 
 namespace FizzBuzzApp.Api
 {
@@ -15,12 +17,30 @@ namespace FizzBuzzApp.Api
         }
 
         [Function("GetFizzBuzz")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "GET", "POST")] HttpRequest req)
         {
             string numberStr = req.Query["number"];
-            int number = int.Parse(numberStr);
-            string result = _fizzBuzzService.GetFizzBuzz(number);
-            return new OkObjectResult(result);
+            if (req.Method == "GET" && numberStr != null)
+            {
+                int number = int.Parse(numberStr);
+                string result = _fizzBuzzService.GetFizzBuzz(number);
+                return new OkObjectResult(result);
+            }
+
+            if (req.Method == "POST")
+            {
+                using var streamReader = new StreamReader(req.Body);
+                string requestBody = await streamReader.ReadToEndAsync();
+                var rangeRequest = JsonConvert.DeserializeObject<FizzBuzzRangeRequest>(requestBody);
+
+                var results = new List<string>();
+                for (int i = rangeRequest.Start.Value; i <= rangeRequest.End.Value; i++)
+                {
+                    results.Add(_fizzBuzzService.GetFizzBuzz(i));
+                }
+                return new OkObjectResult(results);
+            }
+            return new BadRequestObjectResult("Error. Invalid input.");
         }
     }
 }
---
+++ FizzBuzzApp.Domain/Models/Models.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzApp.Domain.Models
+{
+    public class FizzBuzzRangeRequest
+    {
+        public int? Start {  get; set; }
+        public int? End { get; set; }
+    }
+}
